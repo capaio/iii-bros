eb37e5d58c09e7f174a7753cf4532d4170848afb
Carlo Cappai <carlocappai@gmail.com>
2024-08-27 17:22:18 +0200

    fix

--- webpack.config-cordova.js
+++ webpack.config-cordova.js
@@ -43,7 +43,7 @@ module.exports = {
         }),
         new CopyWebpackPlugin({
             patterns: [
-                { from: `./assets/cordova/${process.env.ICON}`, to: `${process.env.ICON}` },
+                { from: `./assets/cordova/${process.env.ICON}`, to: `icon.jpeg` },
                 { from: `./assets/imgs/${process.env.COLLECTIBLE_IMAGE}`, to: `${process.env.COLLECTIBLE_IMAGE}` },
                 { from: `./assets/imgs/${process.env.BUSH_IMAGE}`, to: `${process.env.BUSH_IMAGE}` },
                 { from: `./assets/imgs/${process.env.CLOUD_IMAGE}`, to: `${process.env.CLOUD_IMAGE}` },
